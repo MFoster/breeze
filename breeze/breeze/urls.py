56a45d8e28fb4e438f3b05d9aec1b3ab329ea734
Holly Fionte <hfionte@Hollys-MacBook-Pro.local>
2013-06-23 13:50:19 -0400

    adding new project called breeze, with some basic add/edit forms

---
+++ breeze/breeze/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+from tasks.views import TaskCreate, TaskUpdate, TaskDetailView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'breeze.views.home', name='home'),
+    # url(r'^breeze/', include('breeze.foo.urls')),
+
+    url(r'^task/add/$', TaskCreate.as_view(), name='task_add'),
+    url(r'^task/update/(?P<slug>[-_\w]+)/$', TaskUpdate.as_view(), name='task_update'),
+	url(r'^task/(?P<slug>[-_\w]+)/$', TaskDetailView.as_view(), name='task_detail'),
+
+    #url(r'task/delete/(?P<slug>[-_\w]+)/$$', TaskDelete.as_view(), name='task_delete'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)

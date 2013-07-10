8bf8a056fa55313fbb97d679175c3bdc964e878d
matt foster <mattfoster01@gmail.com>
2013-07-10 13:14:01 -0400

    Finished up super basic crud and basic api test ground to play with it

--- breeze/api/urls.py
+++ breeze/api/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 from api import views
 
 urlpatterns = patterns('',
-
-	url(r'^$', views.index, name='index')
-
+	url(r'^$', views.index, name='api_index'),
+    url(r'^v1/activity/(\d+)$', views.activitySave, name="activity_save"),
+	url(r'^v1/activity', views.index, name="activity_index"),	
+	url(r'^test$', views.test, name='api_test')
 )
--- breeze/api/views.py
+++ breeze/api/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.core import serializers as cereal
+from django.core.context_processors import csrf
+import simplejson as json
 
 from .models import Activity
 
 def index(request):
   collection = Activity.objects.all()
   return HttpResponse(cereal.serialize("json", collection))
+  
+def version(request):
+  registry = [{ "version" : "v1", "url" : "/api/v1/activity"}]
+  
+def activityIndex(request):
+  if request.method == "POST":
+    return HttpResponse("create")
+  
+  collection = Activity.objects.all()
+  return HttpResponse(cereal.serialize("json", collection))
+
+def activitySave(request, arg):
+  if request.method == "PUT":
+    modelData = json.loads(request.raw_post_data)
+    Activity.objects.filter(id=modelData['id']).update(**modelData)
+    return HttpResponse("update")
+  elif request.method == "DELETE":
+    modelData = json.loads(request.raw_post_data)
+    Activity.objects.filter(id=modelData['id']).remove()
+    return HttpResponse("delete")
+  elif request.method == "GET":
+    model = Activity.objects.filter(id=modelData['id'])
+    return HttpResponse(cereal.serialize("json", model))
+
+  
+def test(request):
+  params = { }
+  params.update(csrf(request))
+  return render_to_response('breeze/api-test.html', params)
+  
+  
